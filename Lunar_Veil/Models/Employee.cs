124741fa37ea7968dc1c2cb3d330da75d70fe3c2
Ryuu8462 <longlenguyenhoang959@gmail.com>
2025-07-26 09:19:00 +0700

    Approximate Search by Name or Phone

---
+++ Lunar_Veil/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lunar_Veil.Models;
+
+public class Employee
+{
+    public int EmployeeId { get; set; }
+    public string EmployeeName { get; set; }
+    public string Department { get; set; }
+    public int Gender { get; set; }
+    public string PhotoPath { get; set; } // Add this property to resolve the error  
+    public object PhotoImagePath { get; internal set; }
+    public object Salary { get; internal set; }
+    public object Phone { get; internal set; }
+    public object Email { get; internal set; }
+}
